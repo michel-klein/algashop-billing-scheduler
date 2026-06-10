bf169beaed67f7bf315be3eed8ae44dbb0abff2c
Michel Klein <michel.kps@outlook.com>
2026-06-10 18:55:02 -0300

    feat: billing expired invoices short-lived microservice

--- src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoicesRunner.java
+++ src/main/java/com/algaworks/algashop/billingscheduler/infrastructure/CancelExpiredInvoicesRunner.java
@@ -3,18 +3,19 @@ package com.algaworks.algashop.billingscheduler.infrastructure;
 import com.algaworks.algashop.billingscheduler.application.CancelExpiredInvoicesApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class CancelExpiredInvoicesScheduler {
+public class CancelExpiredInvoicesRunner implements ApplicationRunner {
 
     private final CancelExpiredInvoicesApplicationService applicationService;
 
-    @Scheduled(fixedRate = 5000) //a cada 5 segundos
-    public void runTask() {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
         log.info("Task started - Cancelling expired invoices.");
         applicationService.cancelExpiredInvoices();
         log.info("Task ended - Expired invoices.");
